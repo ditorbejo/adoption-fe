a979d9a0540ef608b2f87b1cc4a425169255c4b3
ditorbejo <ditoooriq@gmail.com>
2023-07-02 22:00:28 +0700

    menambahkan daftar list kategori, daftar kucing by kategori user

---
+++ src/views/DaftarKucingByKategoriUserView.vue
@@ -0,0 +1,96 @@
+<script setup>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const token = localStorage.getItem('token')
+const route = useRoute()
+const itemId = route.params.id
+const pets = ref({})
+const render = async () => {
+  try {
+    const responsePetsByCategory = await axios.get(
+      `http://127.0.0.1:8000/api/pets?categories_id=${itemId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+    if (responsePetsByCategory.status == 200) {
+      console.log(responsePetsByCategory.data.data)
+      pets.value = responsePetsByCategory.data.data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+const router = useRouter()
+function goToDetail(routePath){
+    router.push(routePath)
+}
+
+onMounted(() => {
+  render()
+})
+</script>
+
+<template>
+  <main>
+    <h1>Daftar Kucing by Category</h1>
+
+    <div class="container-list">
+      <div
+        class="container-detail"
+        v-for="pet in pets"
+        :key="pet.id"
+        @click="goToDetail(`/user/pets/${pet.id}`)"
+      >
+        <img :src="`http://127.0.0.1:8000${pet.image}`" alt="" />
+
+        <p>Nama: {{ pet.name }}</p>
+        <p>Color: {{ pet.color }}</p>
+        <p>Category: {{ pet.categories_name }}</p>
+        <p class="status-adopt">{{ pet.status_adopt }}</p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  background-color: white;
+  color: black;
+  width: 100%;
+  padding: 10px 20px;
+  h1 {
+    margin-bottom: 25px;
+  }
+  .container-list {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    .container-detail {
+      display: flex;
+      flex-direction: column;
+      background-color: #ffd482;
+      padding: 10px;
+      gap: 5px;
+      img {
+        width: 100%;
+        height: 300px;
+        object-fit: fill;
+      }
+      .status-adopt {
+        background-color: #85a675;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 5px;
+        font-size: medium;
+        text-transform: capitalize;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/DaftarListKategoriUserView.vue
@@ -0,0 +1,79 @@
+<script setup>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const token = localStorage.getItem('token')
+const categories = ref({})
+const render = async () => {
+  try {
+    const responseCategory = await axios.get(`http://127.0.0.1:8000/api/categories`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    if (responseCategory.status == 200) {
+      console.log(responseCategory.data.data)
+      categories.value = responseCategory.data.data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+const router = useRouter();
+function goToCategory (routePath){
+    router.push(routePath)
+}
+
+onMounted(() => {
+  render()
+})
+</script>
+
+<template>
+  <main>
+    <h1>List Kategori Ras Kucing</h1>
+
+    <div class="container-list">
+      <div class="container-category" v-for="category in categories" :key="category.id" @click="goToCategory(`/user/category/${category.id}`)">
+        <p>{{ category.namecategory }}</p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+* {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  font-family: 'Open Sans', sans-serif;
+}
+main {
+  background-color: white;
+  color: black;
+  width: 100%;
+  padding: 10px 20px;
+  h1{
+    margin-bottom: 5%;
+  }
+  .container-list{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    .container-category{
+        background-color: #FFD482;
+        display: flex;
+        justify-content: center;
+        padding: 5px;
+        border-radius: 5px;
+        cursor: pointer;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+    }
+    .container-category:hover{
+        opacity: 80%;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    }
+  }
+}
+</style>
