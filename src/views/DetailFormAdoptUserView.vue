a7fc65e1d8cb0dfe446d50c8c63269d65e97675e
ditorbejo <ditoooriq@gmail.com>
2023-07-25 17:34:05 +0700

    styling text area dan status form

--- src/views/DetailFormAdoptUserView.vue
+++ src/views/DetailFormAdoptUserView.vue
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import router from '../router'
 
 const token = localStorage.getItem('token')
 const route = useRoute()
@@ -22,7 +23,9 @@ const render = async () => {
     console.log(error)
   }
 }
-
+const goToDetailKucing = (routePath) => {
+  router.push(routePath)
+}
 onMounted(() => {
   render()
 })
@@ -33,6 +36,17 @@ onMounted(() => {
     <h1>Detail Form Adopt</h1>
     <div class="container-nama-kucing">
       <p>{{ dataForm.pet_name }}</p>
+    </div>
+
+    <div class="container-image-kucing">
+      <img :src="`http://127.0.0.1:8000${dataForm.pet_image}`" alt="" />
+    </div>
+
+    <div class="container-button-detail-kucing">
+      <button class="button-detail-kucing" @click="goToDetailKucing(`/pets/${dataForm.pet_id}`)">
+        <i class="fa-solid fa-circle-info fa-xl"></i>
+        Detail Informasi Kucing
+      </button>
     </div>
 
     <div class="container-detail">
@@ -49,18 +63,35 @@ onMounted(() => {
       <p>{{ dataForm.phone_adopter }}</p>
 
       <label for="">Mengapa ingin mengadopsi kucing tersebut</label>
-      <textarea name="" id="" cols="50" rows="10" v-model="dataForm.description" disabled></textarea>
+      <textarea
+        name=""
+        id=""
+        cols="50"
+        class="description"
+        rows="10"
+        v-model="dataForm.description"
+        disabled
+      ></textarea>
     </div>
 
     <div class="container-status">
       <label for="">Status Adopt</label>
-      <p class="status-adopt" v-if="dataForm.status_adopt == 'ready'">{{ dataForm.status_adopt }}</p>
+      <p class="status-adopt" v-if="dataForm.status_adopt == 'ready'">
+        {{ dataForm.status_adopt }}
+      </p>
       <p class="status-adopt-adopted" v-else>{{ dataForm.status_adopt }}</p>
 
       <label for="">Status Form</label>
       <p class="status-form-review" v-if="dataForm.status == 'review'">{{ dataForm.status }}</p>
-      <p class="status-form-approved"  v-else-if="dataForm.status == 'approve'">{{ dataForm.status }}</p>
-      <p class="status-form-unavailable"  v-else>{{ dataForm.status }}</p>
+      <p class="status-form-approved" v-else-if="dataForm.status == 'approve'">
+        {{ dataForm.status }}
+      </p>
+      <p class="status-form-unavailable" v-else>{{ dataForm.status }}</p>
+    </div>
+
+    <div class="alasan-reject" v-if="dataForm.status == 'reject'">
+      <label for="">Alasan Form Ditolak</label>
+      <textarea name="" id="" cols="50" rows="10" v-model="dataForm.reject" disabled></textarea>
     </div>
   </main>
 </template>
@@ -77,62 +108,109 @@ main {
   color: black;
   width: 100%;
   padding: 10px 20px;
-  margin: 0 auto;
-  max-width: 1920px;
-  .container-nama-kucing{
+  margin: 50px auto 0 auto;
+  max-width: 1200px;
+  .container-nama-kucing {
     display: flex;
-    p{
-        display: flex;
-        padding: 5px;
-        border-radius: 5px;
-        justify-content: center;
-        width: 100%;
-        background-color: #FFAE11;
+    p {
+      display: flex;
+      padding: 5px;
+      border-radius: 5px;
+      justify-content: center;
+      width: 100%;
+      background-color: #ffae11;
     }
   }
-  .container-detail{
-    background-color: #FFD482;
+
+  .container-image-kucing {
+    display: flex;
+    background-color: #ffae11;
+    padding: 10px;
+    margin-top: 5%;
+    border-radius: 10px;
+    img {
+      object-fit: fill;
+      width: 100%;
+      height: 300px;
+      border-radius: 10px;
+    }
+  }
+
+  .container-button-detail-kucing {
+    margin-top: 5%;
+    display: flex;
+    button {
+      padding: 10px;
+      width: 100%;
+      border-radius: 10px;
+      background-color: #f29727;
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+        rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+      cursor: pointer;
+    }
+    button:hover {
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    }
+  }
+  .container-detail {
+    background-color: #ffd482;
     display: flex;
     flex-direction: column;
     gap: 5px;
     padding: 5px;
     margin-top: 5%;
     border-radius: 10px;
-    p{
-        background-color: #D9D9D9;
-        padding: 5px;
-        border-radius: 5px;
-        text-transform: capitalize;
+    p {
+      background-color: #d9d9d9;
+      padding: 5px;
+      border-radius: 5px;
+      text-transform: capitalize;
+    }
+    .description {
+      padding: 10px;
+      width: 100%;
+      overflow: hidden;
+      resize: none;
     }
   }
-  .container-status{
+  .container-status {
     margin-top: 5%;
     display: flex;
     flex-direction: column;
     gap: 5px;
-    p{
-        
-        display: flex;
-        justify-content: center;
-        padding: 5px;
-        border-radius: 5px;
-        text-transform: capitalize;
+    p {
+      display: flex;
+      justify-content: center;
+      padding: 5px;
+      border-radius: 5px;
+      text-transform: capitalize;
     }
-    .status-adopt{
-        background-color: #9BB05D;
+    .status-adopt {
+      background-color: #9bb05d;
     }
-    .status-adopt-adopted{
-        background-color: red;
+    .status-adopt-adopted {
+      background-color: red;
     }
-    .status-form-review{
-        background-color: #AFB1B6;
+    .status-form-review {
+      background-color: #afb1b6;
     }
-    .status-form-approved{
-        background-color: #9BB05D;
+    .status-form-approved {
+      background-color: #9bb05d;
     }
 
-    .status-form-unavailable{
-        background-color: red;
+    .status-form-unavailable {
+      background-color: red;
+    }
+  }
+  .alasan-reject {
+    margin-top: 5%;
+    display: flex;
+    flex-direction: column;
+    background-color: #d9d9d9;
+    padding: 10px;
+    border-radius: 10px;
+    textarea {
+      padding: 10px;
     }
   }
 }
