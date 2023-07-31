4626522c0460c21a8402aebd2753929e14acc47f
ditorbejo <ditoooriq@gmail.com>
2023-07-31 08:58:30 +0700

    menambahakan renderform adoptions berdasarkan detail pet

--- src/views/DetailKucingAdminView.vue
+++ src/views/DetailKucingAdminView.vue
@@ -72,8 +72,24 @@ const fetchAlbum = async () => {
       }
     })
     if (responseAlbum.status == 200) {
-      console.log(responseAlbum.data.data)
       images.value = responseAlbum.data.data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const listFormAdoptions = ref({})
+const renderFormAdopt = async () => {
+  try {
+    const renderFormAdopt = await axios.get(`http://127.0.0.1:8000/api/pets/${petId}/adoptions`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    if (renderFormAdopt.status == 200) {
+      console.log(renderFormAdopt.data.data)
+      listFormAdoptions.value = renderFormAdopt.data.data
     }
   } catch (error) {
     console.log(error)
@@ -126,9 +142,16 @@ const hapusGambar = async (imageId) => {
 const goToEdit = (routePath) => {
   router.push(routePath)
 }
+const goToDetailForm = (routePath) => {
+  router.push(routePath)
+}
+const goToListAllFormAdoptions = (routePath) => {
+  router.push(routePath)
+}
 
 onMounted(() => {
   render()
+  renderFormAdopt()
 })
 </script>
 
@@ -203,6 +226,38 @@ onMounted(() => {
       <p class="status_adopt-adopted" v-else>{{ pets.status_adopt }}</p>
     </div>
 
+    <div class="container-adoptions" v-if="listFormAdoptions.length != 0">
+      <p>List Form Adoptions</p>
+
+      <div class="container-button-list-all">
+        <button @click="goToListAllFormAdoptions(`/admin/pets/${petId}/adoptions`)">
+          List All Form Adoptions
+        </button>
+      </div>
+
+      <div class="container-list-form">
+        <div class="detail-form-adoptions" v-for="adoption in listFormAdoptions" :key="adoption.id">
+          <label>Name Adopter</label>
+          <p>
+            {{ adoption.name_adopter }}
+          </p>
+          <label>Phone Adopter</label>
+          <p>
+            {{ adoption.phone_adopter }}
+          </p>
+          <label>Email Adopter</label>
+          <p>
+            {{ adoption.email }}
+          </p>
+          <label>Status Form</label>
+          <p class="adoption-status">
+            {{ adoption.status }}
+          </p>
+          <button @click="goToDetailForm(`/admin/adoptions/${adoption.id}`)">Detail Form</button>
+        </div>
+      </div>
+    </div>
+
     <div class="container-button" v-if="pets.status_adopt == 'ready'">
       <button class="btn-adopt" type="button" @click="goToEdit(`/admin/pets/adopt/${petId}`)">
         Mark As Adopt
@@ -250,7 +305,7 @@ main {
     .gambar-utama {
       padding: 10px;
       background-color: #f79327;
-      width: 100%;
+      width: 300px;
       max-height: 300px;
       object-fit: fill;
     }
@@ -325,6 +380,56 @@ main {
       }
     }
   }
+  .container-adoptions {
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
+    .container-button-list-all {
+      button {
+        padding: 10px;
+        border-radius: 10px;
+        width: 100%;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+          rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+      }
+      button:hover {
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+      }
+    }
+    .container-list-form {
+      margin-top: 10px;
+      display: flex;
+      flex-direction: column;
+      .detail-form-adoptions {
+        display: flex;
+        flex-direction: column;
+        padding: 5px;
+        border-radius: 10px;
+        background-color: #ffd482;
+        p {
+          text-transform: capitalize;
+          padding: 5px;
+          border-radius: 10px;
+          background-color: #f7f3f3;
+        }
+        .adoption-status {
+          text-align: center;
+          background-color: #d9d9d9;
+        }
+        button {
+          margin-top: 10px;
+          border-radius: 10px;
+          padding: 5px;
+          cursor: pointer;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+        }
+        button:hover {
+          box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+        }
+      }
+    }
+  }
   .container-button {
     display: flex;
     flex-direction: column;
@@ -368,6 +473,15 @@ main {
         }
       }
     }
+    .container-adoptions {
+      .container-list-form {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(auto, 1fr);
+        grid-column-gap: 20px;
+        grid-row-gap: 20px;
+      }
+    }
   }
 }
 @media only screen and (min-width: 1024px) {
@@ -390,6 +504,15 @@ main {
           grid-column-gap: 20px;
           grid-row-gap: 20px;
         }
+      }
+    }
+    .container-adoptions {
+      .container-list-form {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: repeat(auto, 1fr);
+        grid-column-gap: 20px;
+        grid-row-gap: 20px;
       }
     }
   }
