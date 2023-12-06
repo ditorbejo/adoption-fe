c8d3df0484a6a999895d663d2eb9bc76d44dab21
ditorbejo <ditoooriq@gmail.com>
2023-12-06 14:29:51 +0700

    fix padding navbar

--- src/App.vue
+++ src/App.vue
@@ -413,7 +413,7 @@ header {
         align-items: center;
         justify-content: center;
         width: 80%;
-        padding: 10%;
+        padding: 5%;
         border: 1px solid black;
         box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px,
           rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
